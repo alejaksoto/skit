f88e88889f0591308e9fe3c8ea14d106f3a4cc02
AlejakSoto <alejaksoto@gmail.com>
2019-11-27 15:56:48 -0500

    update

--- Car.java
+++ Car.java
@@ -4,4 +4,14 @@ class Car {
   String driver;
   String passenger;
 
+  public Car(String license,Account driver){
+      this.license = license;
+      this.driver = driver;
+      
+  }
+
+  void printDataCar(){
+    System.out.println("license" + license + " driver"+ driver);
+  }
+
 }
--- main.java
+++ main.java
@@ -1,9 +1,16 @@
-public static void main(String[] args) {
   class main{
     public static void main (String []  args)
     System.out.println("Hola mundo")
+    Car car = new Car();
+    car.license = "AMQ124";
+    car.driver = "Andres herrera";
+    car.passenger = 4;
+    car.printDataCar ();
+
+    Car car2 = new Car();
+    car2.license = "QW1234";
+    car2.driver = "Andrea herrera";
+    car2.passenger = 3;
+    car2.printDataCar ();
 
   }
-
-
-}
