f8642048d1a2ca636de085d5d33abb76444a1301
AlejakSoto <alejaksoto@gmail.com>
2019-11-27 15:56:21 -0500

    update

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from car import Car
+
 if __name__== "__main__":
         print("hola mundo")
+    car = Car ()
+    car.license = "AXC213"
+    car.driver = "Andres gomez"
+    print (vars (car))
+
+    car2 = Car ()
+    car2.license = "AXC213"
+    car2.driver = "Andres gomez"
+    print (vars (car2))
